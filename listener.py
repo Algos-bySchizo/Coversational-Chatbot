017b57c65b517224e412c33978fe37d2a4e6c278
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2026-04-22 15:30:36 +0500

    created a file that does the recording part and saving the file

---
+++ listener.py
@@ -0,0 +1,34 @@
+import sounddevice as sd
+import scipy.io.wavfile as wav
+import numpy as np
+import threading
+
+def record_audio(sample_rate = 44100):
+    
+    print('\nGo ahead I\'m listening..... Press enter to send when you\'re done\n')
+    
+    frames = []
+    
+    stop_event = threading.Event()
+    
+    
+    def callback(indata, _f, _t, _status):
+        
+        frames.append(indata.copy())
+        
+        if stop_event.is_set():
+            raise sd.CallbackStop
+        
+    with sd.InputStream(samplerate=sample_rate, channels=1, dtype="int16" ,callback=callback):
+        
+        input()
+        
+        stop_event.set()
+
+    audio = np.concatenate(frames, axis = 0)
+
+    wav.write("audio.wav", sample_rate, audio)
+
+    print('Recording Done!')
+    return "audio.wav"
+    
