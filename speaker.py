e7d399ce348b1249748d5a2b609a73924740a71e
Muhammad Usman Junaidy <usmanjunaidy2002@gmail.com>
2026-04-23 14:16:47 +0500

    modify: speech speed

--- speaker.py
+++ speaker.py
@@ -3,7 +3,7 @@ import pyttsx3
 def speak(text):
     engine = pyttsx3.init() # stars the text to speech engine 
     
-    engine.setProperty("rate",160) #rate = speed of speech 175 = words per minute
+    engine.setProperty("rate",155) #rate = speed of speech 175 = words per minute
 
     engine.setProperty("volume",1.0)
 
